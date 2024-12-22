dfdd8fb45d79b6b2ecc4cb8850b1b4b6f69ae615
alexa <alexa@ALEXANDRA_NANU>
2024-12-22 19:55:34 +0200

    url

--- Nanu_Alexandra_Lab11/ListPage.xaml.cs
+++ Nanu_Alexandra_Lab11/ListPage.xaml.cs
@@ -1,3 +1,4 @@
+using Nanu_Alexandra_Lab11.Models;
 namespace Nanu_Alexandra_Lab11;
 
 public partial class ListPage : ContentPage
@@ -6,4 +7,17 @@ public partial class ListPage : ContentPage
 	{
 		InitializeComponent();
 	}
+    async void OnSaveButtonClicked(object sender, EventArgs e)
+    {
+        var slist = (ShopList)BindingContext;
+        slist.Date = DateTime.UtcNow;
+        await App.Database.SaveShopListAsync(slist);
+        await Navigation.PopAsync();
+    }
+    async void OnDeleteButtonClicked(object sender, EventArgs e)
+    {
+        var slist = (ShopList)BindingContext;
+        await App.Database.DeleteShopListAsync(slist);
+        await Navigation.PopAsync();
+    }
 }
